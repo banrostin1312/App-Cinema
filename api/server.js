0340298cc95256f097f87cd2096fda4fb0f97960
Andres Rodriguez <banrostin@gmail.com>
2024-09-11 11:52:49 -0600

    Configurar vercel para deploy y carpeta api back end

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-const router = require('./routes');
-const movieServices = require("./services/movieServices");
+const router = require('../back/src/routes');
+const movieServices = require("../back/src/services/movieServices");
+const frontApi = process.env.FRONTEND_URL || "*";
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Origin', frontApi);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
--- back/index.js
+++ back/index.js
@@ -1,10 +1,10 @@
-const app =  require("./src/server");
+const app =  require("../api/server");
 const dbCon = require ("./src/config/dbCon");
 
-dbCon().then(
-    res=> {
-    app.listen(3000,()=>{
-    console.log("servidor Escuchando desde el puerto 3000");
-})    
-}
-)
+// dbCon().then(
+//     res=> {
+//     app.listen(3000,()=>{
+//     console.log("servidor Escuchando desde el puerto 3000");
+// })    
+// }
+// )
